4721333e5f8bf502ff8d18878811732e61582067
mpapuc <77414775+mpapuc@users.noreply.github.com>
2021-01-23 19:35:37 +0300

    Add project files.

---
+++ Dictionary/MyDictionary.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictionary
+{
+    class MyDictionary<T1, T2>
+    {
+        T1[] sozlukEng;
+        T1[] tempSozlukEng;
+        T2[] sozlukTur;
+        T2[] tempSozlukTur;
+
+        public MyDictionary()
+        {
+            sozlukEng = new T1[0];
+            sozlukTur = new T2[0];
+        }
+
+        public void Add(T1 Eng, T2 Tur)
+        {
+            tempSozlukEng = sozlukEng;
+            tempSozlukTur = sozlukTur;
+
+            sozlukEng = new T1[sozlukEng.Length + 1];
+            sozlukTur = new T2[sozlukTur.Length + 1];
+            for (int i = 0; i < tempSozlukEng.Length; i++)
+            {
+                sozlukEng[i] = tempSozlukEng[i];
+                sozlukTur[i] = tempSozlukTur[i];
+            }
+            sozlukEng[sozlukEng.Length - 1] = Eng;
+            sozlukTur[sozlukTur.Length - 1] = Tur;
+        }
+
+        public int Count
+        {
+            get { return sozlukEng.Length; }
+        }
+
+        public T1[] Eng
+        {
+            get { return sozlukEng; }
+        }
+
+        public T2[] Tur
+        {
+            get { return sozlukTur; }
+        }
+    }
+}
---
+++ Dictionary/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary <string, string> myDictionary = new MyDictionary<string, string>();
+            myDictionary.Add("Book", "Kitap");
+            myDictionary.Add("Table", "Masa");
+            myDictionary.Add("Notebook", "Defter");
+            myDictionary.Add("Paper", "Kağıt");
+            myDictionary.Add(Console.ReadLine(),Console.ReadLine());
+            
+            foreach (var Eng in myDictionary.Eng)
+            {
+                Console.WriteLine(Eng);
+                
+            }
+            foreach (var Tur in myDictionary.Tur)
+            {
+                Console.WriteLine(Tur);
+            }
+
+            Console.WriteLine(myDictionary.Count);
+        }
+    }
+}
